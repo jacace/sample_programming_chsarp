3a823e5cede871da15173e6ca7337911d7057d80
jacace <javierandrescaceres@gmail.com>
2022-01-27 14:47:47 +0000

    added sample queue sort implementation

--- queue/Queue.cs
+++ queue/Queue.cs
@@ -1,14 +1,18 @@
+using System;
+
 public class Queue
 {
 
     private Node head;
     private Node tail;
+    private int size = 0;
 
     public void Enqueue(int num)
     {
         Node n = new Node();
         n.Value = num;
         n.Next = null;
+        size++;
 
         if (head == null)
         {
@@ -22,6 +26,7 @@ public class Queue
                 if (cur.Next == null)
                 {
                     cur.Next = n;
+                    tail = n;
                     break;
                 }
                 else
@@ -32,9 +37,42 @@ public class Queue
         }
     }
 
-    public object Dequeue()
+    public int Dequeue()
     {
-        return null;
+        if (tail != null)
+        {
+            Node cur = head;
+            int returnVal = tail.Value;
+
+            if (tail == head)
+            {
+                head = null;
+                tail = null;
+            }
+            else
+            {
+                while (cur != null)
+                {
+                    if (cur.Next == tail)
+                    {
+                        cur.Next = null;
+                        tail = cur;
+                        break;
+                    }
+                    else
+                    {
+                        cur = cur.Next;
+                    }
+                }
+            }
+
+            size--;
+            return returnVal;
+        }
+        else
+        {
+            throw new Exception("Queue is empty");
+        }
     }
 
     public int Top()
@@ -44,7 +82,34 @@ public class Queue
 
     public void Sort()
     {
-        //TODO
+        //there are limited options incl:
+        // move to a linear structure, sort and copy back
+        // quadratic/recursive
+        // keep it sorted (during insertion)
+        //that's because we're using singly linked list (no indexes)
+
+        int[] tempArr = new int[size];
+        Node cur = head;
+        int i = 0;
+
+        while (cur != null)
+        {
+            tempArr[i] = cur.Value;
+            i++;
+            cur = cur.Next;
+        }
+
+        Array.Sort(tempArr);
+        cur = head;
+        i = 0;
+
+        while (cur != null)
+        {
+            cur.Value=tempArr[i];
+            i++;
+            cur = cur.Next;
+        }
+
     }
 
 }
--- queue/QueueTest.cs
+++ queue/QueueTest.cs
@@ -7,8 +7,12 @@ public class QueueTest
         q.Enqueue(8);
         q.Enqueue(-1);
         q.Enqueue(5);
-
         q.Sort();
 
+        int topVal = q.Dequeue();
+        topVal = q.Dequeue();
+        topVal = q.Dequeue();
+        topVal = q.Dequeue();
+        //q.Dequeue();//raises exception    
     }
 }
